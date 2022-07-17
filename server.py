94122d911f52407ad46ee985754c4db7bc14455c
jinwoo95 <jinwoo95@ajou.ac.kr>
2022-07-17 22:36:57 +0900

    1

--- server.py
+++ server.py
@@ -629,4 +629,4 @@ def Calculate_FoodRecommendedQuantity(food_recommend_quantity, height, weight, a
                 
     return target_nutrient
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port= 80 , debug=True)
